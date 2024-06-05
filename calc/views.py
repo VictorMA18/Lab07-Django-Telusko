345e6e6183518672022f033c8f4b5f5e8ded521a
VictorMA18 <vmamanian@unsa.edu.pe>
2024-06-04 22:49:44 -0500

    Completamos la parte donde sumamos 2 numeros en django para esto hacemos un metodo en views que es add y en urls hacemos su referencia a la direccion la cual usara este metodo y devolvera una nueva pagina con el resultado que es result.html

--- calc/views.py
+++ calc/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html', {'name':'Victor'})
+
+def add(request):
+
+    valor1 = int(request.GET['num1'])
+    valor2 = int(request.GET['num2'])
+
+    res = valor1 + valor2
+
+    return render(request, "result.html", {'resultado':res})
